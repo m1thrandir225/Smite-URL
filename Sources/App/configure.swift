4716d3c60b56eace62a435c09b6b428810ef54e3
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-07-18 01:21:16 +0200

    initial commit

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,9 +1,13 @@
 import Vapor
+import Redis
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // register routes
+	
+	
+	app.redis.configuration = try RedisConfiguration(hostname: "localhost")
     try routes(app)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,50 @@
 import Vapor
+import RediStack
+
+import CryptoKit
+import Foundation
+import Base58Swift
+import BigInt
+
+extension Digest {
+	var bytes: [UInt8] { Array(makeIterator())}
+	var data: Data { Data(bytes)}
+	
+	var hexStr: String {
+		bytes.map {
+			String(format: "%02X", $0)
+		}.joined()
+	}
+}
+
+func sha256f(input: String) -> Data {
+	let dataFromString = input.data(using: .utf8)!
+	let digest = SHA256.hash(data: dataFromString)
+	
+	return digest.data
+}
+
+func base58Encoded(bytes: [UInt8]) -> String {
+	let encodedString = Base58.base58Encode(bytes)
+	
+	return encodedString
+}
+
+
+func generateShortLink(initialLink: String) -> String {
+	let combinedString = initialLink
+	let urlHashBytes = sha256f(input: combinedString)
+	let generatedNumber = BigUInt(urlHashBytes).description
+	
+	let generatedBytes = [UInt8](Data(generatedNumber.utf8))
+	let finalString = base58Encoded(bytes: generatedBytes)
+	return  String(finalString.prefix(8))
+}
+
+//func getFromCache(key: String) -> String {
+//
+//}
+
 
 func routes(_ app: Application) throws {
     app.get { req async in
@@ -8,4 +54,20 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+	app.get("short", ":link") { req -> EventLoopFuture<String> in
+		let initialLink = req.parameters.get("link")!
+		let cacheKey = initialLink // Use the initialLink directly as the key
+		
+		return req.redis.get(RedisKey(cacheKey)).flatMap { result in
+			if let result = result.string {
+				return req.eventLoop.future(result)
+			} else {
+				let generatedShortLink = generateShortLink(initialLink: initialLink)
+				return req.redis.set(RedisKey(cacheKey), toJSON: generatedShortLink).flatMap {
+					return req.eventLoop.future(generatedShortLink)
+				}
+			}
+		}
+	}
+
 }
