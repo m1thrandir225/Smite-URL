ac2580e31916d85a48bfa27c911530139bc16110
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-07-25 13:54:29 +0200

    feat: heroku files added

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -2,22 +2,26 @@
 import XCTVapor
 
 final class AppTests: XCTestCase {
-    var app: Application!
-    
-    override func setUp() async throws {
-        self.app = try await Application.make(.testing)
-        try await configure(app)
-    }
-    
-    override func tearDown() async throws { 
-        try await self.app.asyncShutdown()
-        self.app = nil
-    }
-    
-    func testHelloWorld() async throws {
-        try await self.app.test(.GET, "hello", afterResponse: { res async in
-            XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
-        })
-    }
+	var app: Application!
+	
+	override func setUp() async throws {
+		self.app = try await Application.make(.testing)
+		try await configure(app)
+	}
+	
+	override func tearDown() async throws {
+		try await self.app.asyncShutdown()
+		self.app = nil
+	}
+	
+	func testHelloWorld() async throws {
+		try await self.app.test(.GET, "hello", afterResponse: { res async in
+			XCTAssertEqual(res.status, .ok)
+			XCTAssertEqual(res.body.string, "Hello, world!")
+		})
+	}
+	
+	func testCreateShortURL() async throws {
+		
+	}
 }
